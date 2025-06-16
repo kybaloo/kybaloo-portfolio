f2d9d63d33fd94b3c4db8e47d7cadf4c750b9bd8
Florentin Tchangai <tchangaiflorentin6@gmail.com>
2025-06-16 10:26:33 +0000

    feat: Add image remote patterns to Next.js configuration for enhanced image handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "justtotaltech.com",
+      },
+      {
+        protocol: "https",
+        hostname: "refine-web.imgix.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
